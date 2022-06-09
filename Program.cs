6278010ec66d1067959f361d9d6b178cb62a9d18
Ak <79897291+0vm@users.noreply.github.com>
2022-06-10 01:04:28 +1000

    Uploading Modified Files
    Added Changing Title, Cleaned Code, Shows Timeouts In Title-Bar, gives the ability to log timeouts with date timestamps.
    
    the original was just a multithreaded fast pinger.

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write($"{logMessage} at: ");
+            w.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int Timeouts = 0;
+
+            new Thread(() =>
+            {
+
+                var s = "        "; // 8 spaces for the titles spacing, looks very messy but who cares.
+
+                fortnite:
+                Thread.CurrentThread.IsBackground = true;
+                for (int i = 0; i < int.MaxValue; i++)
+                //{
+                //    Console.Title = $"Rapid IP Pinger | Time open : {i} seconds";
+                //    Thread.Sleep(1000);
+                //}
+                {
+                    var random = new Random();
+                    var randomNumber = random.Next(1, 5);
+
+                    if (randomNumber == 3)
+                    {
+                        Console.Title = "Hey bb "+s+" " +s+s+s+s+s+s+s+s+s+s+s+s+$"  Time open: {i} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                        Console.Title = "Hey bb "+s+" "+s+s+s+s+s+s+s+s+s+s+s+s+$"  Time open: {i+1} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                    }
+                    else if (randomNumber == 2)
+                    {
+                        Console.Title = "Rapid IP / Host Pinger "+s+" "+s+s+s+s+s+s+s+s+s+$"Time open: {i} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                        Console.Title = "Rapid IP / Host Pinger "+s+" "+s+s+s+s+s+s+s+s+s+$"Time open: {i+1} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                    }
+                    else if (randomNumber == 1)
+                    {
+                        Console.Title = "Consider Starring The Repo? github.com/0vm/Pinger "+s+" "+s+s+$" Time open: {i} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                        Console.Title = "Consider Starring The Repo? github.com/0vm/Pinger "+s+" "+s+s+$" Time open: {i+1} seconds"+s+"|"+s+$"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                    }
+                    else if (randomNumber == 4)
+                    {
+                        Console.Title = "Pro Tip: Click On The Console To Pause The Pinger   " + s + " " + s + s + $"    Time open: {i} seconds" + s + "|" + s + $"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                        Console.Title = "Pro Tip: Click On The Console To Pause The Pinger   " + s + " " + s + s + $"    Time open: {i + 1} seconds" + s + "|" + s + $"Total Timeouts: {Timeouts}";
+                        Thread.Sleep(500);
+                    }
+                }
+            }).Start();
+
+            int? log = 0;
+
+            Console.Write("Want to log the timeouts to a file? (y/n): ");
+            ConsoleKey response = Console.ReadKey(false).Key;
+            Console.WriteLine();
+            if (response == ConsoleKey.Y)
+            {
+               log = 1;
+            }else
+            {
+                log = 0;
+            }
+            Console.Clear();
+            Console.Write("Enter Host: ");
+            string option = Console.ReadLine();
+
+
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                while (true)
+                {
+                    try
+                    {
+                        int timeout = 1500;
+                        Ping ping = new Ping();
+                        PingReply pingreply = ping.Send(option, timeout);
+                        if (pingreply.Status == IPStatus.Success)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Status: {0}", pingreply.Status);
+                            Console.ForegroundColor = ConsoleColor.White; Console.Write(" | ");
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.Write("IP: {0}", pingreply.Address);
+                            Console.ForegroundColor = ConsoleColor.White; Console.Write(" | ");
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("Latency: {0}ms", pingreply.RoundtripTime);
+                            Console.WriteLine();
+                        }else
+                        { 
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(option + " Timed Out");
+
+                            if (log == 1)
+                            {
+                                using (StreamWriter w = File.AppendText("log.txt"))
+                                {
+                                    Log($"{option} Timed Out", w);
+                                }
+                            }
+                            Timeouts++;
+                            continue;
+                        }
+                    }
+                    catch (PingException ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }).Start();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rapid IP Pinger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IP Pinger?")]
+[assembly: AssemblyCopyright("Copyright © 0vm & zf9 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e503346-e531-4ee3-a777-087d4a89bc14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
